a56fd37513194b68b74f6cfaab33fcd5678b212f
KianTheCon <hamzeik2786@my.sduhsd.net>
2023-06-17 20:09:22 -0700

    Update mcstatuscheck.py

--- mcstatuscheck/mcstatuscheck.py
+++ mcstatuscheck/mcstatuscheck.py
@@ -13,7 +13,25 @@ class MCStatusCheck(commands.Cog):
         """This does stuff!"""
         # Your code will go here
         server = JavaServer.lookup(server_ip);
-        latency = server.ping()
-        print(f"The server replied in {latency} ms")
-        SEmbed = discord.Embed(title = 'Players Online',description = "```yaml\n" + str(latency) + "\n```",colour=discord.Color.from_rgb(21,244,238))
-        await ctx.send(f"The server replied in {latency} ms")
+        try:
+            status = server.status()
+            latency = str(status.latency)
+            SEmbed = discord.Embed(description = "```yaml\n+SERVER ONLINE+\nPlayers: {0}\nPing: {1} ms\n```".format(status.players.online, status.latency), colour=discord.Color.from_rgb(0,255,0))
+        except:
+            SEmbed = discord.Embed(description="```diff\n-SERVER OFFLINE-\n```", colour=discord.Color.from_rgb(255, 0, 0))
+        await ctx.send(embed=SEmbed)
+
+    @commands.command()
+    async def players(self, ctx, server_ip: str):
+        server = JavaServer.lookup(server_ip);
+        query = server.query()
+        if query.players.names:
+            SEmbed = discord.Embed(title = 'Players Online',description = "```yaml\n" + "\n".join(query.players.names) + "\n```",colour=discord.Color.from_rgb(21,244,238))
+        else:
+            status = server.status()
+
+            if not status.players.sample:
+                SEmbed = discord.Embed(description = "```diff\n-NO PLAYERS ONLINE-\n```",colour=discord.Color.from_rgb(255,0,0))
+            else:
+                SEmbed = discord.Embed(title = 'Players Online',description = "```yaml\n" + "\n".join([player.name for player in status.players.sample]) + "\n```",colour=discord.Color.from_rgb(21,244,238))
+        await ctx.send(embed=SEmbed)
